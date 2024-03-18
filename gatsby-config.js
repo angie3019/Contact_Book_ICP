3df4a52e2193ac3577be7e2faf61544cd9d8c9c8
Anjali <singlanjali@gmail.com>
2024-03-18 16:43:01 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `contact_book`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
